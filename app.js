b2252463a5ad5b86e070abd43e681cac53c5b5b3
Ismet Kovačević <kovacevicismet@gmail.com>
2019-05-23 11:26:32 +0200

    Auth: email/password #1 (#2)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const expressValidator = require('express-validator')
 const routes = require('./routes')
 
 const app = express()
@@ -9,6 +10,8 @@ app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(expressValidator())
 
 app.use('/', routes)
 
