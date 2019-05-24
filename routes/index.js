9b92d8e9b25e52e0faa272f34c047c5e0fe79fb2
Ismet Kovačević <kovacevicismet@gmail.com>
2019-05-24 14:16:24 +0200

    Auth: OAuth flow facebook #1 (#3)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
-const passportService = require('../handlers/passport')
 const authController = require('../controllers/authContorller')
 
 const requireAuth = passport.authenticate('jwt', { session: false })
 const requireSignin = passport.authenticate('local', { session: false })
+const facebookSignin = passport.authenticate('facebook', { session: false })
 
 // auth
 router.post(
@@ -15,5 +15,7 @@ router.post(
 )
 router.post('/api/auth/signin', requireSignin, authController.signIn)
 router.get('/api/auth/me', requireAuth, authController.getCurrentUser)
+router.get('/api/auth/facebook', facebookSignin)
+router.get('/api/auth/facebook/callback', facebookSignin, authController.signIn)
 
 module.exports = router
